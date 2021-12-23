7fd5bb795aa01dcf6b35fad97a19c6a69f676d14
jamesleonard929292 <96542249+jamesleonard929292@users.noreply.github.com>
2021-12-23 00:58:23 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -90,7 +90,7 @@ print(greeter.functions.greet().call())
 
 
 nonce = W3.eth.getTransactionCount(address1)
-tx_dict = greeter.functions.setGreeting('Nihao').buildTransaction({
+tx_dict = greeter.functions.setGreeting('My name is James Leonard and my student number is 10571088').buildTransaction({
   'chainId': 3,
   'gas': 1400000,
   'gasPrice': w3.toWei('40', 'gwei'),
